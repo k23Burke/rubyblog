6615a8404c71df5469ec50341926effcb20554d6
Kyle Burke <k23burke@gmail.com>
2015-10-02 11:25:52 -0400

    edit pages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,15 @@
 class ArticlesController < ApplicationController
-	def new
-	end
-
 	def index
 		@articles = Article.all
 	end
 
+	def new
+		@article = Article.new
+	end
+
 	def create
 		@article = Article.new(article_params)
- 
+
 		if @article.save
 			redirect_to @article
 		else
@@ -19,6 +20,20 @@ class ArticlesController < ApplicationController
 
 	def show
 		@article = Article.find(params[:id])
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def update
+		@article = Article.find(params[:id])
+
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render 'edit'
+		end
 	end
 
 	private
